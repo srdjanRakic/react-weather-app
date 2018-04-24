518fa6fc14fe13796f7a65e6b0945a9ef2b987a6
Srdjan Rakic <srgjan.rakic@gmail.com>
2018-04-24 15:39:13 +0200

    Remove unused components

--- src/components/Details/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const DescriptionContainer = styled.div`
-    font-size: 34px;
-    font-weight: 100;
-    max-width: 400px;
-    margin: 0 auto;
-    text-align: center;
-`;
